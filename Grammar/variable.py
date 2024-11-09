a900621cb7aa57a82950f8f62fa90b411228fdf2
jin0w0 <hwangjiin0709@gmail.com>
2024-11-09 18:13:45 +0900

    (type)추가

--- Grammar/variable.py
+++ Grammar/variable.py
@@ -1,7 +1,23 @@
-score = 0
+score = 0 #variable
 print(score)
+print(type(score)) #data type
+score1 = 1.234
+print(score1)
+print(type(score1))
 alphabet = "abc"
 print(alphabet)
+print(type(alphabet))
 print(alphabet*2)
 list0 = [1, 2, 3]
 print(list0)
+print(type(list0))
+me = {'height':160, 'weight':45}
+print(me)
+print(type(me))
+bool1 = True
+bool2 = False
+print(bool1)
+print(not bool1)
+print(bool1 and bool2)
+print(bool1 or bool2) #Logical Operators(not, and, or)
+print(type(bool1))
