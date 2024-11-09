956f85713af82d1859625e6b7b3097cc58c0a1f5
jin0w0 <hwangjiin0709@gmail.com>
2024-11-09 20:26:09 +0900

    for문

--- Grammar/ControlFlow.py
+++ Grammar/ControlFlow.py
@@ -6,3 +6,12 @@ elif x<0:
     print("x<0")
 else:
     print("x=0")
+#for
+list1 = ['a','b','c']
+for i in list1:
+    print(i)
+dict1 = {'a':'가','b':'나','c':'다'}
+for key, value in dict1.items():
+    print(key, value)
+for i in range(0, 3, 1):
+    print(i)
