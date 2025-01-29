fab6992289e6bc51d429653a05243b867c416faf
jin0w0 <hwangjiin0709@gmail.com>
2025-01-29 17:56:54 +0900

    .

--- Grammar/list.py
+++ Grammar/list.py
@@ -1,13 +1,17 @@
 a = []
 print('a:', a, 'type(a):', type(a))
+
 x = ['apple', 'orange']
 print('x:', x, 'x[0]:',x[0],'x[-1]:', x[-1])
+
 y = ['carrot', 'potato']
 print('y:', y)
+
 z = [x, y]
 print('z=[x,y]:', z)
 print('z[0][1]:', z[0][1])
 print('len(z):', len(z))
+
 num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 print('num[0:4]:', num[0:4], 'num[4:]:', num[4:])
 print('num[0:9:3]:', num[0:9:3], 'num[9:0:-1]:',num[9:0:-1], 'num[::-1]:', num[::-1])
--- Grammar/variable.py
+++ Grammar/variable.py
@@ -1,19 +1,24 @@
 score = 0 #variable
 print(score)
 print(type(score)) #data type
+
 score1 = 1.234
 print(score1)
 print(type(score1))
+
 alphabet = "abc"
 print(alphabet)
 print(type(alphabet))
 print(alphabet*2)
+
 list0 = [1, 2, 3]
 print(list0)
 print(type(list0))
+
 me = {'height':160, 'weight':45}
 print(me)
 print(type(me))
+
 bool1 = True
 bool2 = False
 print(bool1)
